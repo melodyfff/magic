58e728edc5e6b155de1255618d3cc659f48cb2e3
melodyfff <307208327@qq.com>
2020-06-02 17:50:34 +0800

    :smile: update

---
+++ vue/hello-world/src/components/ComponentTest.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <span>Message: {{ msg }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponetTest',
+    props: {
+        msg: String
+    }
+}
+</script>
--- vue/hello-world/src/views/Test.vue
+++ vue/hello-world/src/views/Test.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an test page</h1>
+    <ComponetTest msg="ComponentTest" />
   </div>
 </template>
+
+
+<script>
+
+import ComponetTest from '@/components/ComponentTest.vue'
+
+export default {
+  name: 'Test',
+  components: {
+    ComponetTest
+  }
+
+}
+</script>
